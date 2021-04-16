920a08a8f35329fb18e16c1b3fe7334c7ca3d18a
John Clyde <john.clyde@mozartdata.com>
2021-04-16 14:28:55 -0700

    Remove all the javascript.

--- setup.py
+++ setup.py
@@ -28,15 +28,7 @@ class EggInfoWithJS(egg_info):
         if os.path.exists(static_path) or "READTHEDOCS" in os.environ:
             pass
         else:
-            js_path = "sqllineagejs"
             use_shell = True if platform.system() == "Windows" else False
-            subprocess.check_call(
-                shlex.split("npm install"), cwd=js_path, shell=use_shell
-            )
-            subprocess.check_call(
-                shlex.split("npm run build"), cwd=js_path, shell=use_shell
-            )
-            shutil.move(os.path.join(js_path, STATIC_FOLDER), static_path)
         super().run()
 
 
