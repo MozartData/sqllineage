ff762b6193ca14a185ec922fca15e9e695c3355a
John Clyde <john.clyde@mozartdata.com>
2021-04-29 15:59:16 -0700

    Bump version to 100.0.9 - 8 was a botched release.

--- sqllineage/__init__.py
+++ sqllineage/__init__.py
@@ -16,7 +16,7 @@ def _monkey_patch() -> None:
 _monkey_patch()
 
 NAME = "sqllineage"
-VERSION = "100.0.8a"
+VERSION = "100.0.9a"
 DEFAULT_LOGGING = {
     "version": 1,
     "disable_existing_loggers": False,
